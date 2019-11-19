c28979873b4e3a68a88cc179bd45c5f5931f79ad
cjd2951 <cjd2951@gmail.com>
2019-11-19 16:28:45 -0700

    initial working extension (#8)
    * initial extension (#2)
    
    Adds initial extension integration, gradle requirements for packaging, and ignores intellij config files
    
    * Cdavis/feature/initial ext (#3)
    
    * initial extension
    
    Adds initial extension integration, gradle requirements for packaging, and ignores intellij config files
    
    * don't ignore required jars

--- src/main/java/edu/msudenver/bluejext/qualitytools/Library.java
+++ src/main/java/edu/msudenver/bluejext/qualitytools/Library.java
@@ -1,10 +1,92 @@
-/*
- * Example source code for the Quality Tools for BlueJ project.
- */
 package edu.msudenver.bluejext.qualitytools;
 
-public class Library {
-    public boolean someLibraryMethod() {
+import bluej.extensions.*;
+import bluej.extensions.event.*;
+
+import java.net.URL;
+
+/**
+ * This is the starting point of the BlueJ Extension
+ *
+ * @see bluej.extensions.event.PackageListener
+ * @see bluej.extensions.Extension
+ */
+public class Library extends Extension implements PackageListener {
+
+    /**
+     * @param bluej instance of BlueJ this extension should register with
+     */
+    @Override
+    public void startup(BlueJ bluej) {}
+
+    /**
+     * @param ev package event containing BlueJ event information
+     */
+    @Override
+    public void packageOpened(PackageEvent ev) {}
+
+    /**
+     * @param ev package event containing project information
+     */
+    @Override
+    public void packageClosing(PackageEvent ev) {}
+
+    /**
+     * @return If this Extension is compatible with the current release of the BlueJ Extensions API
+     */
+    @Override
+    public boolean isCompatible() {
+        //TODO implement proper compatibility logic
         return true;
+    }
+
+    /**
+     * @return The version number of this extension
+     */
+    @Override
+    public String getVersion() {
+        //TODO make this version a project property
+        return ("0.0.01");
+    }
+
+    /**
+     * @return The user-visible name of this extension
+     */
+    @Override
+    public String getName() {
+        //TODO make this name a project property
+        return ("Quality Tools");
+    }
+
+    /**
+     * Terminates the extension
+     */
+    @Override
+    public void terminate() {
+        //TODO implement terminate
+    }
+
+    /**
+     * @return The description of the extension
+     */
+    @Override
+    public String getDescription() {
+        //TODO make this description a project property
+        return ("BlueJ extension that integrates quality assessment tools");
+    }
+
+    /**
+     * @return A URL where you can find info on this extension.
+     */
+    @Override
+    public URL getURL() {
+        try {
+            //TODO make this URL a project property
+            return new URL("https://github.com/MetroCS/QualityToolsForBlueJ");
+        } catch (Exception e) {
+            // The link is either dead or otherwise unreachable
+            System.out.println("Quality Tools: getURL: Exception=" + e.getMessage());
+            return null;
+        }
     }
 }
--- src/test/java/edu/msudenver/bluejext/qualitytools/LibraryTest.java
+++ src/test/java/edu/msudenver/bluejext/qualitytools/LibraryTest.java
@@ -1,14 +1,51 @@
-/*
- * Unit test class example for the Quality Tools for BlueJ project.
- */
 package edu.msudenver.bluejext.qualitytools;
 
+import org.junit.Before;
 import org.junit.Test;
-import static org.junit.Assert.*;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class LibraryTest {
-    @Test public void testSomeLibraryMethod() {
-        Library classUnderTest = new Library();
-        assertTrue("someLibraryMethod should return 'true'", classUnderTest.someLibraryMethod());
+
+    Library unit;
+
+    @Before
+    public void setup() {
+        unit = new Library();
+    }
+
+    @Test
+    public void isCompatible() {
+        assertTrue(unit.isCompatible());
+    }
+
+    @Test
+    public void getVersion() {
+        assertEquals("0.0.01", unit.getVersion());
+    }
+
+    @Test
+    public void getName() {
+        assertEquals("Quality Tools", unit.getName());
+    }
+
+    @Test
+    public void terminate() {
+        unit.terminate();
+    }
+
+    @Test
+    public void getDescription() {
+        assertEquals("BlueJ extension that integrates quality assessment tools", unit.getDescription());
+    }
+
+    @Test
+    public void getURL() throws MalformedURLException {
+        URL url = new URL("https://github.com/MetroCS/QualityToolsForBlueJ");
+        assertEquals(url, unit.getURL());
     }
 }
